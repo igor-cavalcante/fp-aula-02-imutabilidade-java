ca3a2070e890e0cbb477a2cdad73b8cbcc21ffd5
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-02-03 18:05:51 -0300

    Criando cidade mutavel

--- src/main/java/exercicios/Cidade.java
+++ src/main/java/exercicios/Cidade.java
@@ -1,5 +1,17 @@
 package exercicios;
 
-public record Cidade(String nome) {
+public class Cidade{
+    private String nome;
 
+    public Cidade(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
